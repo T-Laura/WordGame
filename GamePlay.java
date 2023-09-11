cfff49d2438569aad4730f0d3baa498357e2e114
T-Laura <143134620+T-Laura@users.noreply.github.com>
2023-09-11 14:55:35 -0700

    Add files via upload

--- GamePlay.java
+++ GamePlay.java
@@ -2,33 +2,39 @@ import java.util.Scanner;
 
 public class GamePlay
 {
-   private static Players player;
+   private static Players[] currentPlayers;
    private static Hosts host;
    private static Turn turn;
    
    public static void main(String[] args){
       String firstName;
       String lastName;
+      int turnNumber = 0;
       boolean numberGuessed;
       Scanner scnr = new Scanner(System.in);
+      currentPlayers = new Players[3];
       host = new Hosts("Bob", "Barker");
       turn = new Turn();
       
-      System.out.println("What is your first name?");
-      firstName = scnr.nextLine();
-      System.out.println("Would you like to enter a last name? Leave blank if not");
-      lastName = scnr.nextLine();
-      if (lastName == ""){
-         player = new Players(firstName);
-      }
-      else{
-         player = new Players(firstName, lastName);
+      for (int i = 0; i < 3; i++) {
+         System.out.println("What is player " + (i + 1) + "'s first name?");
+         firstName = scnr.nextLine();
+         System.out.print("Would you like to enter a last name for player " + (i + 1));
+         System.out.println("? Leave blank if not");
+         lastName = scnr.nextLine();
+         if (lastName == ""){
+            currentPlayers[i] = new Players(firstName);
+         }
+         else{
+            currentPlayers[i] = new Players(firstName, lastName);
+         }
       }
       do{
          host.randomizeNum();
          numberGuessed = false;
          while (!numberGuessed){
-            numberGuessed = turn.takeTurn(player, host, scnr);
+            numberGuessed = turn.takeTurn(currentPlayers[turnNumber % 3], host, scnr);
+            turnNumber++;
          }
          System.out.println("Play another game? (y or n)");
       } while (scnr.nextLine().equals("y"));
